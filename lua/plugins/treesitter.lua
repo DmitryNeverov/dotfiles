52112eabff4464802bbaa2673340e3886d565671
Dmitry Neverov <mail@dmitryneverov.xyz>
2025-09-03 19:53:02 +0900

    added treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-treesitter/nvim-treesitter", branch = 'master',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function ()
+        require("nvim-treesitter.configs").setup({
+            -- parsers you care about
+	    ensure_installed = { "lua", "markdown", "python" },
+            auto_install = true,
+	})
+    end,
+}
